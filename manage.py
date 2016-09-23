24fdbb5af9cc65124f2af5b49aa74bd0e7c65bad
Cavin <xjr30226@126.com>
2016-09-23 09:27:42 +0800

    再次整改，尚未完成

--- manage.py
+++ manage.py
@@ -1,16 +1,13 @@
 #!/usr/bin/env python3
 #-*- coding:utf-8 -*-
-from pprint import pprint
 
-from Flask import flask
+from app import create_app
 from flask_script import Manager, Shell
-import pymysql
-import requests
-import re
+
+app = create_app('default')
+manager = Manager(app)
+
+manager.add_command("shell")
 
 if __name__ == '__main__':
-    c = check_check()
-    date, from_station, to_station = c.get_station_info()
-    print(date, from_station, to_station)
-    piao = c.check_rest_fee(date, from_station, to_station)
-    pprint(piao)
+    manager.run()
